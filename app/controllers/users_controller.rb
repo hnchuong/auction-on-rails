0def0794e471d1c3239243e502c68c7268a90815
Chuong Huynh <hnchuong@gmail.com>
2025-03-18 15:22:50 +0700

    add base registration and home

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  allow_unauthenticated_access only: %i[ new create ]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Account created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
+  end
+end
