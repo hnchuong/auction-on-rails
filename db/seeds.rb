1cf230ec759c6f38e9bc5e42a1a4b06f5f7c103e
Chuong Huynh <hnchuong@gmail.com>
2025-03-18 14:58:18 +0700

    add built-in authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create(
+  name: "User",
+  email_address: "user@example.com",
+  password: "password",
+  password_confirmation: "password",
+  role: "BUYER")
