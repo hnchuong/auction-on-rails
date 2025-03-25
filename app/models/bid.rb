9e821fa6398fb24fed1491afddd1c418ad13716f
Chuong Huynh <hnchuong@gmail.com>
2025-03-25 19:55:25 +0700

    fix

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,9 +3,12 @@ class Bid < ApplicationRecord
   belongs_to :buyer, class_name: 'User'
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :auction, :buyer, presence: true
 
   validates_each :amount do |record, attr, value|
+    next if record.auction.nil?
+
     current_price = record.auction.current_price || record.auction.starting_price
-    record.errors.add(attr, 'must be greater than the current price') if record.auction.current_price && value <= current_price
+    record.errors.add(attr, 'must be greater than the current price') if value <= current_price
   end
 end
