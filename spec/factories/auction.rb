843ecdc65aad8211ccaa3c151d818069e46736ae
Chuong Huynh <hnchuong@gmail.com>
2025-03-25 21:17:42 +0700

    fix auction spec

--- spec/factories/auction.rb
+++ spec/factories/auction.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :auction do
     title { "Test Auction" }
     start_time { Time.now }
-    starting_price { 100 }
+    starting_price { 90 }
     duration { 60 }
     status { "INACTIVE" }
     association :seller, factory: :user
